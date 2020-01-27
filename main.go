371f1a65e1718ba9cd51b09ed9806f3d3ddc5a42
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-01-28 00:26:58 +0500

    --Commit: user type added

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/ibadsiddiqui/APIs-For-Shahrukh/src/handlers"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.IndexHandler)
+	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	controllers.executeAllControllers()
 	http.ListenAndServe(":8080", nil)
 }
